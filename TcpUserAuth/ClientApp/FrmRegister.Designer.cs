357149fdba81063b79e9a314750cd32de335e41e
PVL-cyber <lnpvy0516@gmail.com>
2025-10-27 00:15:30 +0700

    Update FrmRegister.Designer.cs

--- TcpUserAuth/ClientApp/FrmRegister.Designer.cs
+++ TcpUserAuth/ClientApp/FrmRegister.Designer.cs
@@ -2,15 +2,9 @@
 {
     partial class FrmRegister
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
+       
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,12 +14,7 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
@@ -42,9 +31,7 @@
             this.label6 = new System.Windows.Forms.Label();
             this.textBox5 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
+         
             this.label1.AutoSize = true;
             this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.label1.Font = new System.Drawing.Font("Verdana", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -66,9 +53,7 @@
             this.label2.Size = new System.Drawing.Size(229, 36);
             this.label2.TabIndex = 1;
             this.label2.Text = "Tên đăng nhập";
-            // 
-            // label3
-            // 
+        
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.SystemColors.ControlLight;
@@ -77,9 +62,7 @@
             this.label3.Size = new System.Drawing.Size(145, 36);
             this.label3.TabIndex = 2;
             this.label3.Text = "Mật khẩu";
-            // 
-            // label4
-            // 
+            
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.SystemColors.ControlLight;
@@ -99,37 +82,27 @@
             this.label5.Size = new System.Drawing.Size(108, 36);
             this.label5.TabIndex = 4;
             this.label5.Text = "Họ tên";
-            // 
-            // textBox1
-            // 
+          
             this.textBox1.Location = new System.Drawing.Point(400, 138);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(270, 22);
             this.textBox1.TabIndex = 5;
-            // 
-            // textBox2
-            // 
+          
             this.textBox2.Location = new System.Drawing.Point(400, 202);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(270, 22);
             this.textBox2.TabIndex = 6;
-            // 
-            // textBox3
-            // 
+            
             this.textBox3.Location = new System.Drawing.Point(400, 271);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(270, 22);
             this.textBox3.TabIndex = 7;
-            // 
-            // textBox4
-            // 
+            
             this.textBox4.Location = new System.Drawing.Point(400, 341);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(270, 22);
             this.textBox4.TabIndex = 8;
-            // 
-            // button1
-            // 
+            
             this.button1.BackColor = System.Drawing.Color.Plum;
             this.button1.Font = new System.Drawing.Font("Bookman Old Style", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.Location = new System.Drawing.Point(325, 464);
@@ -139,9 +112,7 @@
             this.button1.Text = "ĐĂNG KÝ";
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
+            
             this.button2.BackColor = System.Drawing.Color.Plum;
             this.button2.Font = new System.Drawing.Font("Bookman Old Style", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.Location = new System.Drawing.Point(568, 464);
@@ -151,9 +122,7 @@
             this.button2.Text = "HỦY";
             this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // label6
-            // 
+           
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.ForeColor = System.Drawing.SystemColors.ControlLight;
@@ -162,17 +131,12 @@
             this.label6.Size = new System.Drawing.Size(93, 36);
             this.label6.TabIndex = 11;
             this.label6.Text = "Email";
-            // 
-            // textBox5
-            // 
+          
             this.textBox5.Location = new System.Drawing.Point(400, 409);
             this.textBox5.Name = "textBox5";
             this.textBox5.Size = new System.Drawing.Size(270, 22);
             this.textBox5.TabIndex = 12;
-            // 
-            // FrmRegister
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+           this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DarkSlateBlue;
             this.ClientSize = new System.Drawing.Size(959, 531);
@@ -196,7 +160,6 @@
 
         }
 
-        #endregion
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
